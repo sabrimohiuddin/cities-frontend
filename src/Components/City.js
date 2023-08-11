b54a875f43bfbae78819e88d27fd8d7cd19dec94
Frantz-Sebastien Mathias <frantz-sebastienmathias@pursuit.org>
2023-08-11 02:37:10 -0400

    Adding Bootstrap

--- src/Components/City.js
+++ src/Components/City.js
@@ -2,25 +2,24 @@ import { Link } from "react-router-dom";
 
 function City({ city }) {
   return (
-    <tr>
-      <td>
-        {city.has_visited ? (
-          <span>✅</span>
-        ) : (
-          <span>&nbsp; &nbsp; &nbsp;</span>
-        )}
-      </td>
-      <td>{city.city}</td>
-      <td>
-        <img src={city.image} alt={`${city.city} Image`} />
-      </td>
-      <td>{city.country}</td>
-      <td>${city.ticket_price.toFixed(2)}</td>
-      <td>
-        <Link to={`/cities/${city.id}`}>View Comments</Link>
-      </td>
-    </tr>
+    <div className="card" style={{ width: "18rem" }}>
+    <img src={city.image} className="card-img-top" alt={`${city.city} Image`} />
+    <div className="card-body">
+      <h4 className="card-title">{city.city}</h4>
+      <p className="card-title">
+          {city.has_visited ? (
+            <span>Visited ✅</span>
+            ) : (
+              null
+              )}
+      </p>
+      <h6>{city.country}</h6>
+      <p className="card-text">Average Ticket Price: ${city.ticket_price.toFixed(2)}</p>
+      <Link className="btn btn-primary" to={`/cities/${city.id}`}>View Comments</Link>
+    </div>
+  </div>
   );
 }
 
 export default City;
+
