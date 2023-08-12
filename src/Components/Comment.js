f226b3b3d934b11bdd698606d16d0f2f55de0478
Frantz-Sebastien Mathias <frantz-sebastienmathias@pursuit.org>
2023-08-12 07:30:52 -0400

    Comments bug is fixed. Now using a bootstrap code I found on the internet for these comments

--- src/Components/Comment.js
+++ src/Components/Comment.js
@@ -9,28 +9,95 @@ function Comment({ comment, handleDelete, handleSubmit }) {
   };
 
   return (
-    <div className="Comment">
-      <button onClick={toggleView}> {viewEditForm ? 'See comment' : 'Edit this comment'}</button>
+    // <div className="Comment">
 
-      {viewEditForm ? (
-        <CommentForm
-          commentDetails={comment}
-          toggleView={toggleView}
-          handleSubmit={handleSubmit}
-        />
-
-      ) : (
-        <div>
-          <h4>
-            {comment.title}
-          </h4>
-          <h5>{comment.commenter}</h5>
-          <p>{comment.content}</p>
-          <button onClick={() => handleDelete(comment.id)}>delete</button>
-        </div>
-      )}
+    //   {viewEditForm ? (
+    //     <CommentForm
+    //     commentDetails={comment}
+    //     toggleView={toggleView}
+    //     handleSubmit={handleSubmit}
+    //     />
+        
+    //     ) : (
+    //       <div>
+    //       <h4>
+    //         {comment.commenter}
+    //       </h4>
+    //       <h5>{comment.title}</h5>
+    //       <p>{comment.content}</p>
+    //       <button onClick={() => handleDelete(comment.id)}>Delete this comment</button>
+    //       <button onClick={toggleView}> {viewEditForm ? 'See comment' : 'Edit this comment'}</button>
+    //     </div>
+    //   )}
       
+    // </div>
+
+    
+
+<div className="container mt-5">
+
+<div className="row  d-flex justify-content-center">
+
+    <div className="col-md-8">
+
+        {/* <div className="headings d-flex justify-content-between align-items-center mb-3">
+            <h5>Unread comments(6)</h5>
+
+            //So hum... we might need to work on that, but this part is optional
+            
+        </div> */}
+
+
+
+        <div className="card p-3">
+
+            <div className="d-flex justify-content-between align-items-center">
+
+          <div className="user d-flex flex-row align-items-center">
+
+            
+            <span><small className="font-weight-bold text-primary">james_olesenn</small> <small className="font-weight-bold">Hmm, This poster looks cool</small></span>
+              
+          </div>
+
+
+          </div>
+
+
+          <div className="action d-flex justify-content-between mt-2 align-items-center">
+
+            <div className="reply px-4">
+                <small>Remove</small>
+                <span className="dots"></span>
+                <small>Edit</small>
+               
+               
+            </div>
+
+            <div className="icons align-items-center">
+
+                <i className="fa fa-star text-warning"></i>
+                <i className="fa fa-check-circle-o check-icon"></i>
+                
+            </div>
+              
+          </div>
+
+
+            
+        </div>
+
+
+
+        
     </div>
+    
+</div>
+
+</div>
+
+
+
   );
 }
   
